56dd512da6b80b4f27756776bd0c804b66f20e07
Santin20 <santinfabrizio20@gmail.com>
2026-07-13 22:51:39 +0530

    Delete ai story gen directory

--- ai story gen/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from signin import sign
-from dash import dash
-main=Flask(__name__)
-main.secret_key="aigenstory"
-main.register_blueprint(sign)
-main.register_blueprint(dash)
-if __name__ == "__main__":
-    main.run(debug=True)
