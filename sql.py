f63c69f42fd1dbe3ae061a4f5cd0db31f7c850f4
Santin20 <santinfabrizio20@gmail.com>
2026-07-13 22:53:25 +0530

    Add files via upload

---
+++ sql.py
@@ -0,0 +1,12 @@
+import mysql.connector as con
+from dotenv import load_dotenv
+import os
+def connection():
+    load_dotenv()
+    connect=con.connect(
+    host="localhost",
+    user=os.getenv("user"),
+    password=os.getenv("sql"),
+    database="story",
+    charset="utf8mb4")
+    return connect
